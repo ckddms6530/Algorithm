09ee493395d7a1694a7de92eff0b696d1600f690
유창은 <ckddms6530@gmail.com>
2024-06-06 14:17:35 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,8 +1,18 @@
-SELECT count(*) as COUNT
-FROM ECOLI_DATA 
+# SELECT count(*) as COUNT
+# FROM ECOLI_DATA 
+# WHERE 
+#      (GENOTYPE & 2) = 0 
+#     AND 
+#      (GENOTYPE & 5) > 0
+# ;
+-- 조건 걸어줄 필요가 없었음 
+-- 어차피 where 절에선 0이 아니면 True 기 때문에..
+SELECT 
+    count(*) as COUNT
+FROM 
+    ECOLI_DATA 
 WHERE 
-     (GENOTYPE & 2) = 0 
-    AND 
-     (GENOTYPE & 5) > 0
+     NOT GENOTYPE & 2
+     AND 
+     GENOTYPE & 5
 ;
-
